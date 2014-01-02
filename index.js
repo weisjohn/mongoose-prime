bd68e90085e8f0ac34e2b80d37c99104803ddaaa
John Weis <weis.john@gmail.com>
2014-01-02 13:47:07 -0500

    removing non-simple types in the find method

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 
-var async = require('async');
+var async = require('async')
+  , _ = require('lodash')
+  ;
 
 module.exports = function(model, data, cb) {
     var skipped = 0, added = 0, failed = 0;
     async.each(data, function(data, cb) {
-        model.findOne(data, function(err, results) {
+
+        var copy = {};
+        _.chain(data).keys().each(function(key) {
+            var type = typeof data[key];
+            if (type == "object" || type == "function") return;
+            copy[key] = data[key];
+        });
+
+        model.findOne(copy, function(err, results) {
             if (err) return cb(err);
             if (results) { skipped++; return cb(); }
             (new model(data)).save(function(err, results) {
